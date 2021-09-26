137977f9c4fcc32dfcedc9de0f4c837cf0ef8b58
Владислав Куманов <edu.kumanov@outlook.com>
2021-09-27 01:28:08 +0300

    Add project files.

---
+++ Lesson4-Practice2-SequentialCollections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Lesson4_Practice2_SequentialCollections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue();
+
+            queue.Enqueue("First");
+            queue.Enqueue("Second");
+            queue.Enqueue("Third");
+            queue.Enqueue("Fourth");
+
+            while (queue.Count > 0)
+            {
+                Console.WriteLine("From Queue: {0}", queue.Dequeue());
+            }
+
+            Stack stack = new Stack();
+
+            stack.Push("First");
+            stack.Push("Second");
+            stack.Push("Third");
+            stack.Push("Fourth");
+
+            while (stack.Count > 0)
+            {
+                Console.WriteLine("From Stack: {0}", stack.Pop());
+            }
+        }
+    }
+}
